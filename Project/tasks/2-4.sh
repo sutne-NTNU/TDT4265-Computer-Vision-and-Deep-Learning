957cfdf0e7ca0e4dadcc2807289df18b261cf5d2
sutne <sivertutne@gmail.com>
2022-05-08 14:14:53 +0200

    tweak

--- Project/tasks/2-4.sh
+++ Project/tasks/2-4.sh
@@ -1,8 +1,8 @@
 cd src
 
-echo -e "\nTask 2.4 - The Best"                     
+echo -e "\nTask 2.4 - Training The Best"                     
 python3 train.py configs/task_2_4.py # train on small dataset to see if the model is any good
 python3 train.py configs/updated_dataset.py # train on updated dataset before saving results
 
-echo -e "\nTask 2.4 - Saving Validation Results for Scoreboard"   
+echo -e "\nTask 2.4 - Saving Validation Results for Leaderboard"   
 python3 save_validation_results.py configs/updated_dataset.py ../outputs/results.json
